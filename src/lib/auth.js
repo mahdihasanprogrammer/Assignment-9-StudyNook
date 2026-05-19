1b8156aedcc25783053ced0fdbce62e223bc3f83
mahdihasanprogrammer <mahdihasan.code@gmail.com>
2026-05-19 11:20:23 +0600

    add login page design and functionality , and added authentication with google system

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,11 +12,12 @@ export const auth = betterAuth({
   }),
     emailAndPassword: { 
     enabled: true, 
+    autoSignIn: false
   }, 
   socialProviders: { 
-    github: { 
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }, 
   },
 });
